976834485114bde94ef816d7c82ed0192e186966
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-03-22 00:47:46 +0800

    Fix fade on load issue.

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const hamburger = document.querySelector("header > nav > img")
 const navList = document.querySelector("header > nav > ul")
+const navListClose = navList.querySelector("img")
 hamburger.addEventListener("click", () => {
-	navList.classList.toggle("navOpen")
+	navList.style.animation = ""
+	navList.classList.add("navOpen")
+})
+navListClose.addEventListener("click", () => {
+	navList.style.animation = "fadeOut 0.5s forwards ease-in-out"
+	navList.classList.remove("navOpen")
 })
 window.addEventListener("keydown", (e) => {
 	if (e.key === "Escape" && navList.classList.contains("navOpen")) {
+		navList.style.animation = "fadeOut 0.5s forwards ease-in-out"
 		navList.classList.remove("navOpen")
 	}
 })
+
+// close nav modal on desktop view
+const resizeObserver = new ResizeObserver((entries) => {
+	for (let entry of entries) {
+		if (entry.contentBoxSize) {
+			const boxSize = entry.contentBoxSize[0]
+			if (boxSize.inlineSize / boxSize.blockSize >= 1) {
+				navList.style.animation = ""
+				navList.classList.remove("navOpen")
+			}
+		}
+	}
+})
+
+resizeObserver.observe(document.body)
